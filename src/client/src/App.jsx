183752d004b1d38e1e0a5ae1642b426a5d811c30
Anukul Chandra <101039186+AnukulPr1me@users.noreply.github.com>
2024-01-23 12:14:31 +0530

    added files for login, register and Dashboard

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,4 +1,28 @@
 import { useState } from 'react'
+import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+}from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/login',
+    element: <div><Login/></div>
+  },
+  {
+    path:'/register',
+    element: <div><Register/></div>
+  },
+  {
+    path:'/dashboard',
+    element: <div><Dashboard/></div>
+  },
+])
 
 
 function App() {
@@ -6,7 +30,8 @@ function App() {
 
   return (
    <div>
-    Hello
+    <RouterProvider router={router}/>
+    
    </div>
   )
 }
